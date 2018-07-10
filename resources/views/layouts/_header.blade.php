a12e0fd06611e9d58f45ce7b5e81348cbed26597
cymon1997 <cybermonster82@yahoo.com>
2018-07-10 14:51:39 +0700

    added uniform page

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -5,9 +5,15 @@
         </a>
     </div>
     <div class="cell auto text-center">
+        @if(Request::is('uniform'))
+        <a href="{{ route('uniform') }}">
+            <img class="chooser" src="{{ asset('img/Uniform_Title_Text.png') }}" alt="">
+        </a>
+        @else
         <a href="{{ route('index') }}">
             <img class="chooser" src="{{ asset('img/Logo_Text.png') }}" alt="">
         </a>
+        @endif
     </div>
     <div class="cell shrink">
         <a href="#">
--- resources/views/pages/uniform.blade.php
+++ resources/views/pages/uniform.blade.php
@@ -7,8 +7,73 @@
 @endsection
 
 @section('content')
-<div id="searchbar" class="bg-gray">
+<div id="searchbar" class="grid-x bg-gray">
+    <div class="cell small-2 text-left">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Sort_Text.png') }}" alt="">
+        </a>
+    </div>
+    <div class="cell auto text-center">
+        <a href="{{ route('index') }}">
+            <img class="chooser" src="{{ asset('img/Search_Text.png') }}" alt="">
+        </a>
+    </div>
+    <div class="cell small-2 text-right">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Filter_Text.png') }}" alt="">
+        </a>
+    </div>
+</div>
 
+<div id="searchbar-extended" class="grid-x bg-semi-gray">
+    <div class="cell small-2 medium-1 text-left">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Search_Only_Text.png') }}" alt="">
+        </a>
+    </div>
+    <div class="cell auto text-center">
+        <form class="" action="#" method="post">
+            <input class="form-underline form-narrow bg-transparent" type="text" name="search" value="">
+        </form>
+    </div>
+    <div class="cell small-2 medium-1 text-right">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Up_Button.png') }}" alt="">
+        </a>
+    </div>
+</div>
+
+<div id="searchbar-sort" class="grid-x bg-semi-gray">
+    <div class="cell small-2 medium-1 text-left">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Sort_Only_Text.png') }}" alt="">
+        </a>
+    </div>
+    <div class="cell auto text-center">
+        <form class="" action="#" method="post">
+            <fieldset>
+                <input class="bg-transparent" type="radio" name="sort" value="0">
+                <img class="chooser-mini" src="{{ asset('img/Popularity_Text.png') }}" alt="">
+
+                <input class="bg-transparent" type="radio" name="sort" value="1">
+                <img class="chooser-mini" src="{{ asset('img/Low_To_High_Text.png') }}" alt="">
+
+                <input class="bg-transparent" type="radio" name="sort" value="2">
+                <img class="chooser-mini" src="{{ asset('img/High_To_Low_Text.png') }}" alt="">
+
+                <input class="bg-transparent" type="radio" name="sort" value="3">
+                <img class="chooser-mini" src="{{ asset('img/New_In_Text.png') }}" alt="">
+
+                <input class="bg-transparent" type="radio" name="sort" value="4">
+                <img class="chooser-mini" src="{{ asset('img/Discount_Text.png') }}" alt="">
+            </fieldset>
+        </form>
+    </div>
+    <div class="cell small-2 medium-1 text-right">
+        <a href="#">
+            <img class="chooser-mini" src="{{ asset('img/Up_Button.png') }}" alt="">
+        </a>
+    </div>
 </div>
 
 <div id="uniform" class="chooser-bg overlay-container" style="background-image: url({{ asset('img/Uniform_Top.png') }})">
